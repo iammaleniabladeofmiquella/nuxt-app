5abeb753d314c397463396473dd2010102b5648b
Wan • D • Mean <phatthanasak.w@gmail.com>
2022-07-03 11:58:09 +0700

    add sass and sass-loader lib

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>สวัสดีชาวโลก</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, a.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 10px;
+  margin: 10px;
+  h1 {
+    font-size: 28px;
+    font-weight: 700;
+  }
+  p {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+}
+</style>
